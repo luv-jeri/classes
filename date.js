ee3978fbf38aaf30a4d32978e8a6c6daa8c6b65a
luv-jeri <luvvjeri@gmail.com>
2022-04-29 15:31:52 +0530

    -

--- date.js
+++ date.js
@@ -1,13 +1,36 @@
-var greet = (function () {
-  function sayHello() {
-    console.log('Hello');
+var date_module = (function () {
+  function getTodayDate() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+      dd = '0' + dd;
+    }
+    if (mm < 10) {
+      mm = '0' + mm;
+    }
+    today = mm + '/' + dd + '/' + yyyy;
+    return today;
   }
-  function sayGoodBye() {
-    console.log('Good Bye');
+
+  function getTomorrowDate() {
+    var today = new Date();
+    var dd = today.getDate() + 1;
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+      dd = '0' + dd;
+    }
+    if (mm < 10) {
+      mm = '0' + mm;
+    }
+    today = mm + '/' + dd + '/' + yyyy;
+    return today;
   }
 
   return {
-    sayHello: sayHello,
-    sayGoodBye: sayGoodBye,
+    getTodayDate: getTodayDate,
+    getTomorrowDate: getTomorrowDate,
   };
 })();
--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-(function () {
-  // Your code here...
-})();
+// # IIFE : Immediately Invoked Function Expression
+//` ( create your function here )()
+// Arrow function
+// (()=> {
+//   console.log('Hello World Arrow function');
+// })()
+
+// Normal Function
+// (function () {
+//   console.log(`Hello`);
+// })();
+
+console.log(date_module.getTodayDate());
